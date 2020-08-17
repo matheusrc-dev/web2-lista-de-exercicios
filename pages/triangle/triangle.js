518c5c1c63e1e6ed2d5aa860ac8b8df4a114e02d
Matheus Rocha <matheusrochacamarg@gmail.com>
2020-08-16 22:52:05 -0300

    Refactoring triangle.js

--- pages/triangle/triangle.js
+++ pages/triangle/triangle.js
@@ -2,39 +2,32 @@ function triangleType() {
     let lado1 = Number(document.getElementById('side-one').value);
     let lado2 = Number(document.getElementById('side-two').value);
     let lado3 = Number(document.getElementById('side-three').value);
-    let result = document.querySelector("div.result");
-    result.removeChild(result.firstChild);
-    let tipoDoTriangulo;
-    let text
+    
 
     if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
         alert('Todos os valores precisam ser maiores que 0');
         location.reload();
     } else if (lado1 == lado2 && lado2 == lado3) {
-        tipoDoTriangulo = document.createElement('p');
-        text = document.createTextNode('Triângulo equilátero');
-        tipoDoTriangulo.appendChild(text);
+        insertValuesOnDocument('Triângulo equilátero');
 
-        result.appendChild(tipoDoTriangulo);
-        console.log(text);
-        alert(text.textContent);
     } else if ((lado1 == lado2 && lado2 != lado3) || (lado1 == lado3 && lado1 != lado2) || 
             (lado2 == lado3 && lado2 != lado1)) {
+        insertValuesOnDocument('Triângulo isósceles');
 
-        tipoDoTriangulo = document.createElement('p');
-        text = document.createTextNode('Triângulo isósceles');
-        tipoDoTriangulo.appendChild(text);
-
-        result.appendChild(tipoDoTriangulo);
-        console.log(text);
-        alert(text.textContent);
     } else if(lado1 != lado2 && lado1 != lado3) {
-        tipoDoTriangulo = document.createElement('p');
-        text = document.createTextNode('Triângulo escaleno');
-        tipoDoTriangulo.appendChild(text);
-
-        result.appendChild(tipoDoTriangulo);
-        console.log(text);
-        alert(text.textContent);
+        insertValuesOnDocument('Triângulo escaleno');
     }
+}
+
+function insertValuesOnDocument(mensagem) {
+    let result = document.querySelector("div.result");
+    result.removeChild(result.firstChild);
+    
+    let tipoDoTriangulo = document.createElement('p');
+    let text = document.createTextNode(mensagem);
+    tipoDoTriangulo.appendChild(text);
+
+    result.appendChild(tipoDoTriangulo);
+    console.log(text);
+    alert(text.textContent);
 }
