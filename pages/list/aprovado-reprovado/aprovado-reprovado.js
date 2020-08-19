a3189a5804f88c9a262f453dbeae9098dd501ef0
Matheus Rocha <matheusrochacamarg@gmail.com>
2020-08-18 21:53:33 -0300

    Atividade III, Version 2(Anon Functions)

---
+++ pages/list/aprovado-reprovado/aprovado-reprovado.js
@@ -0,0 +1,30 @@
+const minMedia = 6.0, minFreq = 0.75;
+
+function ehAprovado(calcularMedia, calcularPercentagemFrequencia) {
+    let media, percentagemFrequencia;
+
+    let n1 = Number(document.getElementById('nota1').value);
+    let n2 = Number(document.getElementById('nota2').value);
+    
+    let freq = Number(document.getElementById('freq').value);
+    let aulas = Number(document.getElementById('aulas').value);
+    
+    
+    calcularMedia = (function() {
+        media = (n1 + n2) / 2
+    })();
+
+    calcularPercentagemFrequencia = (() => {
+        percentagemFrequencia = freq / aulas;
+    })();
+
+    let result = document.querySelector('div.result');
+    if(media >= minMedia && percentagemFrequencia >= minFreq) {
+        result.innerHTML = `aluno APROVADO `;
+    }else {
+        result.innerHTML = `aluno REPROVADO `;
+    }
+
+    result.innerHTML += `Media final: ${media} Frequencia: ${percentagemFrequencia * 100}%`;
+
+}
