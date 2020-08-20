96c39f67004686a0c8d4807df816d32aab788c40
Matheus Rocha <matheusrochacamarg@gmail.com>
2020-08-19 22:08:28 -0300

    Atividade III Version 4(Anon Functions)

--- pages/list/aprovado-reprovado/aprovado-reprovado.js
+++ pages/list/aprovado-reprovado/aprovado-reprovado.js
@@ -1,29 +1,26 @@
 const minMedia = 6.0, minFreq = 0.75;
+let media;
+let percentagemFrequencia;
 
-function ehAprovado(calcularMedia, calcularPercentagemFrequencia) {
-    let media, percentagemFrequencia;
-
-    let n1 = Number(document.getElementById('nota1').value);
-    let n2 = Number(document.getElementById('nota2').value);
+(function ehAprovado(calcularMedia, calcularPercentagemFrequencia) {
+    let n1 = Number(window.prompt('Nota um'));
+    let n2 = Number(window.prompt('Nota dois'));
+    let aulas = Number(window.prompt('Total de aulas'));
+    let freq = Number(window.prompt('Quantidade de aulas frequentadas'));
     
-    let freq = Number(document.getElementById('freq').value);
-    let aulas = Number(document.getElementById('aulas').value);
-    
-    
-    calcularMedia = (function() {
-        media = (n1 + n2) / 2
-    })();
-
-    calcularPercentagemFrequencia = (() => {
-        percentagemFrequencia = freq / aulas;
-    })();
+    calcularMedia(n1, n2);
+    calcularPercentagemFrequencia(freq, aulas);
 
     let result = document.querySelector('div.result');
-    if(media >= minMedia && percentagemFrequencia >= minFreq) {
+    if (media >= minMedia && percentagemFrequencia >= minFreq) {
         result.innerHTML = `aluno APROVADO `;
-    }else {
+    } else {
         result.innerHTML = `aluno REPROVADO `;
     }
 
     result.innerHTML += `Media final: ${media.toFixed(1)} Frequencia: ${(percentagemFrequencia * 100).toFixed(1)}%`;
-}
+})(function (n1, n2) {
+    media = (n1 + n2) / 2;
+}, (freq, aulas) => {
+    percentagemFrequencia = freq / aulas;
+});
