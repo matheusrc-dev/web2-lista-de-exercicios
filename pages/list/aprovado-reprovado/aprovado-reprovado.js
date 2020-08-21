3577ce2818c7c449a57b25e5525990a6ac7b1e55
Matheus Rocha <matheusrochacamarg@gmail.com>
2020-08-21 00:30:00 -0300

    Atividade III Version 5(Anon Functions)

--- pages/list/aprovado-reprovado/aprovado-reprovado.js
+++ pages/list/aprovado-reprovado/aprovado-reprovado.js
@@ -1,26 +1,31 @@
-const minMedia = 6.0, minFreq = 0.75;
-let media;
-let percentagemFrequencia;
-
-(function ehAprovado(calcularMedia, calcularPercentagemFrequencia) {
-    let n1 = Number(window.prompt('Nota um'));
-    let n2 = Number(window.prompt('Nota dois'));
-    let aulas = Number(window.prompt('Total de aulas'));
-    let freq = Number(window.prompt('Quantidade de aulas frequentadas'));
+function finalDoSemestre () {
+    const minMedia = 6.0, minFreq = 0.75;
+    let calcularMedia = function (n1, n2) {
+        return (n1 + n2) / 2;
+    };
     
-    calcularMedia(n1, n2);
-    calcularPercentagemFrequencia(freq, aulas);
-
-    let result = document.querySelector('div.result');
-    if (media >= minMedia && percentagemFrequencia >= minFreq) {
-        result.innerHTML = `aluno APROVADO `;
-    } else {
-        result.innerHTML = `aluno REPROVADO `;
-    }
-
-    result.innerHTML += `Media final: ${media.toFixed(1)} Frequencia: ${(percentagemFrequencia * 100).toFixed(1)}%`;
-})(function (n1, n2) {
-    media = (n1 + n2) / 2;
-}, (freq, aulas) => {
-    percentagemFrequencia = freq / aulas;
-});
+    let calcularPercentagemFrequencia =  (freq, aulas) => {
+        return freq / aulas;
+    };
+    
+    (function ehAprovado(calcularMedia, calcularPercentagemFrequencia) {
+        let n1 = parseFloat(document.getElementById('nota1').value);
+        let n2 = parseFloat(document.getElementById('nota2').value);
+        let aulas = parseFloat(document.getElementById('aulas').value);
+        let freq = parseFloat(document.getElementById('frequencia').value);
+        let media;
+        let percentagemFrequencia;
+    
+        media = calcularMedia(n1, n2);
+        percentagemFrequencia = calcularPercentagemFrequencia(freq, aulas);
+    
+        let result = document.querySelector('div.result');
+        if (media >= minMedia && percentagemFrequencia >= minFreq) {
+            result.innerHTML = `aluno APROVADO `;
+        } else {
+            result.innerHTML = `aluno REPROVADO `;
+        }
+    
+        result.innerHTML += `Media final: ${media.toFixed(1)} Frequencia: ${(percentagemFrequencia * 100).toFixed(1)}%`;
+    })(calcularMedia, calcularPercentagemFrequencia);
+}
